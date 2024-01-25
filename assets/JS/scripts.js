9c98fc70e5b3c5be82df4c6b5552019bd0126b23
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-01-25 20:06:29 +0100

    inserimento del counter

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,5 +1,58 @@
-let ciao = "ciao sono Davide Poletto";
-console.log(ciao);
+//FUNZIONE PER CREARE BOTTONE
+function creaBottone(testo, gestoreEvento) {
+    let bottone = document.createElement("button");
+    bottone.innerHTML = testo;
 
+    if(gestoreEvento){
+        bottone.addEventListener("click", gestoreEvento);
+    }
+    return bottone;
+}
+
+//H1
 document.getElementById("title").innerHTML="Questo è un contatore di pecore"
+
+//BOTTONI
+const positiveButton = document.getElementById("positive_button");
+const negativeButton = document.getElementById("negative_button");
+const resetButton = document.getElementById("reset_button");
+const startButton = document.getElementById("start_button");
+
+let bottone1 = creaBottone("-", decrementaContatore);
+negativeButton.appendChild(bottone1);
+
+let bottone2 = creaBottone("Reset", resettaContatore)
+resetButton.appendChild(bottone2);
+
+let bottone3 = creaBottone("+", incrementaContatore);
+positiveButton.appendChild(bottone3);
+
+
+//COUNTER
+let contatore = 0;
+let display = document.getElementById("display")
+
+function incrementaContatore(){
+    contatore++;
+    updateDisplay();
+}
+
+function decrementaContatore(){
+    if (contatore > 0){
+    contatore--;
+    updateDisplay();
+    }
+}
+
+function resettaContatore(){
+    contatore = 0;
+    updateDisplay();
+}
+
+function updateDisplay(){
+    display.innerHTML = contatore;
+}
+
+//START IMG
+
 
