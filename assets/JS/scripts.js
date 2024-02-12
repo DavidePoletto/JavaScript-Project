3d8e072ec62e0a85903029fced06c5eb2ab9971d
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-02-12 21:55:27 +0100

    update to js

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,59 +1,65 @@
-//FUNZIONE PER CREARE BOTTONE
-function creaBottone(testo, gestoreEvento) {
-    let bottone = document.createElement("button");
-    bottone.innerHTML = testo;
+const container = document.createElement("div");
+container.id = "container";
+document.body.appendChild(container);
 
-    if(gestoreEvento){
-        bottone.addEventListener("click", gestoreEvento);
-    }
-    return bottone;
-}
+//SFONDO
+const img = document.createElement("img");
+img.id = "big_video";
+img.src = "/assets/IMG/Sheeps.png";
+img.alt = "sheeps gif"
+container.appendChild(img);
 
-//H1
-document.getElementById("title").innerHTML="Sheep counter"
+//HEADER
+const header = document.createElement("section");
+header.className = "header";
+container.appendChild(header);
 
+//HEADER TITLE
+const titleBox = document.createElement("section");
+titleBox.className = "title_box";
+header.appendChild(titleBox);
 
-//BOTTONI
-const positiveButton = document.getElementById("positive_button");
-const negativeButton = document.getElementById("negative_button");
-const resetButton = document.getElementById("reset_button");
-const startButton = document.getElementById("start_button");
+const title = document.createElement("h1");
+title.id = "title";
+title.textContent = "Sheep Counter";
+titleBox.appendChild(title);
 
+//SOCIAL LOGO
+const socialBox = document.createElement("section");
+socialBox.className = "social_box";
+header.appendChild(socialBox);
 
-let bottone1 = creaBottone("-", decrementaContatore);
-negativeButton.appendChild(bottone1);
+const social = document.createElement("div");
+social.id = "social"
+socialBox.appendChild(social);
 
-let bottone2 = creaBottone("Reset", resettaContatore);
-resetButton.appendChild(bottone2);
+const socialLogo = document.createElement("a");
+socialLogo.href = "https://github.com/DavidePoletto";
+social.appendChild(socialLogo);
 
-let bottone3 = creaBottone("+", incrementaContatore, riproduciSUonoCasuale);
-positiveButton.appendChild(bottone3);
-
-
-//AUDIO
-const suono = [
-    '/assets/Audio/sheep1.mp3',
-    '/assets/Audio/sheep2.mp3',
-    '/assets/Audio/sheep3.mp3',
-];
-
-const audioPecore = new Audio();
-
-function riproduciSUonoCasuale() {
-    const indiceCasuale = Math.floor(Math.random() * suono.length);
-    audioPecore.src = suono[indiceCasuale];
-    audioPecore.play();
-};
-
-let audioSfondo = new Audio ('/assets/Audio/bells.mp3')
-audioSfondo.addEventListener("ended", function() {
-    audioSfondo.currentTime = 0;
-    audioSfondo.play();
-});
-
-const audioReset = new Audio('/assets/Audio/reset.mp3')
+const linkImg = document.createElement("img");
+linkImg.src = "/assets/IMG/github.png";
+linkImg.alt = "github icon";
+socialLogo.appendChild(linkImg);
 
 //COUNTER
+const counterBox = document.createElement("section");
+counterBox.className = "counter_box"
+container.appendChild(counterBox);
+
+//COUNTER DISPLAY
+const displaBox = document.createElement("section");
+displaBox.className = "display_box";
+counterBox.appendChild(displaBox);
+
+const display0 = document.createElement("div");
+display0.id = "display";
+displaBox.appendChild(display0);
+
+const displayP = document.createElement("p");
+displayP.textContent = "0";
+display0.appendChild(displayP);
+
 let contatore = 0;
 let display = document.getElementById("display")
 
@@ -102,7 +108,52 @@ function updateDisplay(){
     display.innerHTML = contatore;
 }
 
+//FUNZIONE PER CREARE BOTTONE
+function creaBottone(testo, gestoreEvento) {
+    let bottone = document.createElement("button");
+    bottone.innerHTML = testo;
+
+    if(gestoreEvento){
+        bottone.addEventListener("click", gestoreEvento);
+    }
+    return bottone;
+}
+
+//BOTTONI
+const buttonBox = document.createElement("section");
+buttonBox.className = "button_box";
+container.appendChild(buttonBox);
+
+const negativeButton = document.createElement("div");
+negativeButton.id = "negative_button";
+buttonBox.appendChild(negativeButton);
+
+const resetButton = document.createElement("div");
+resetButton.id = "reset_button";
+buttonBox.appendChild(resetButton);
+
+const positiveButton = document.createElement("div");
+positiveButton.id = "positive_button";
+buttonBox.appendChild(positiveButton);
+
+let bottone1 = creaBottone("-", decrementaContatore);
+negativeButton.appendChild(bottone1);
+
+let bottone2 = creaBottone("Reset", resettaContatore);
+resetButton.appendChild(bottone2);
+
+let bottone3 = creaBottone("+", incrementaContatore, riproduciSUonoCasuale);
+positiveButton.appendChild(bottone3);
+
 //BOTTONE START
+const startBox = document.createElement("section");
+startBox.className = "start_box";
+container.appendChild(startBox);
+
+const startButton = document.createElement("div")
+startButton.id = "start_button";
+startBox.appendChild(startButton);
+
 let bottone4 = creaBottone("Avvia");
 startButton.appendChild(bottone4);
 bottone4.addEventListener("click", function(){
@@ -117,3 +168,28 @@ bottone4.addEventListener("click", function(){
         bottone4.textContent = "Avvia"
     }
 });
+
+//AUDIO
+const suono = [
+    '/assets/Audio/sheep1.mp3',
+    '/assets/Audio/sheep2.mp3',
+    '/assets/Audio/sheep3.mp3',
+];
+
+const audioPecore = new Audio();
+
+function riproduciSUonoCasuale() {
+    const indiceCasuale = Math.floor(Math.random() * suono.length);
+    audioPecore.src = suono[indiceCasuale];
+    audioPecore.play();
+};
+
+let audioSfondo = new Audio ('/assets/Audio/bells.mp3')
+audioSfondo.addEventListener("ended", function() {
+    audioSfondo.currentTime = 0;
+    audioSfondo.play();
+});
+
+const audioReset = new Audio('/assets/Audio/reset.mp3')
+
+
